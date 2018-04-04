2ca17f71edbbbaff774a04228ba7d15d60a35188
unknown <roberto.sl.90@gmail.com>
2018-04-05 00:55:16 +0200

    .

--- src/auth.service.js
+++ src/auth.service.js
@@ -1,26 +1,26 @@
 
 export default class AuthService {
 
-    static getCredentials() {
-        return JSON.parse(window.localStorage.getItem('auth'));
-    }
+  static getCredentials() {
+    return JSON.parse(window.localStorage.getItem('auth'));
+  }
 
-    static hasCredentials() {
-        return window.localStorage.getItem('auth') !== null;
-    }
+  static hasCredentials() {
+    return window.localStorage.getItem('auth') !== null;
+  }
 
-    static deleteCredentials() {
-        window.localStorage.removeItem('auth');
-    }
+  static deleteCredentials() {
+    window.localStorage.removeItem('auth');
+  }
 
-    static getUsername() {
-        return this.hasCredentials()
-            ? this.getCredentials().username
-            : 'Stranger';
-    }
+  static getUsername() {
+    return this.hasCredentials()
+      ? this.getCredentials().username
+      : 'Stranger';
+  }
 
-    static doLogin() {
-        this.deleteCredentials();
-        window.location.href = "/auth";
-    }
+  static doLogin() {
+    this.deleteCredentials();
+    window.location.href = "/auth";
+  }
 }
